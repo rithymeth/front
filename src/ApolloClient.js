e66d83a047ad1dfd4f5aa46c5389d1789e91f388
rithymeth <rithymega1331@gmail.com>
2024-01-09 10:10:53 +0700

    first commit

---
+++ src/ApolloClient.js
@@ -0,0 +1,16 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://apollo-recipe-api-ce0549082988.herokuapp.com/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
